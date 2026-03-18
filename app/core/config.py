ebf3b7f6a1f9848ceb9900dd839702bae5a8edfb
Олійник Гліб <glib070707@gmail.com>
2026-03-18 20:21:16 +0200

    fix: config now reading the .env file

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,25 @@
+from pathlib import Path
+
 from pydantic import model_validator
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file = ROOT_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_URL: str = ""
 
-    @model_validator(mode = "after")
+    @model_validator(mode="after")
     def build_db_url(self):
         self.DB_URL = f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@localhost/{self.DB_NAME}"
         return self
