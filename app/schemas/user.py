a6e6345a5b23a7755ef032942eb22f8e1b98e1ae
Олійник Гліб <glib070707@gmail.com>
2026-03-22 20:56:22 +0200

    feat: create user authenticate method

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -18,3 +18,7 @@ class UserResponse(BaseModel):
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserLogin(UserBase):
+    password: str = Field(min_length=8)
--- app/services/user.py
+++ app/services/user.py
@@ -1,8 +1,10 @@
+from pydantic import EmailStr
+
 from app.repositories.user import UserRepository
 from app.schemas.user import UserCreate
 from app.models.user import User
 from app.exceptions.user import UserExists, UserNotFound
-from app.securities.hashing import hash_password
+from app.securities.hashing import hash_password, verify_password
 
 
 class UserService:
@@ -23,4 +25,14 @@ class UserService:
         user = await self.repo.get_by_id(user_id)
         if user is None:
             raise UserNotFound()
+        return user
+
+    async def authenticate_user(
+        self,
+        user_email: EmailStr,
+        password: str
+    ) -> User | None:
+        user = await self.repo.get_by_email(user_email)
+        if user is None or not verify_password(password, user.password):
+            return None
         return user
