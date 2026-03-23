08256f560b2de84ee0678c5ce57bafc96fdd6abc
Олійник Гліб <glib070707@gmail.com>
2026-03-23 20:41:37 +0200

    feat: create refresh token model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .check import Check
 from .monitor import Monitor
+from .refresh_token import RefreshToken
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy.sql import func
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.database import Base
@@ -15,4 +15,12 @@ class User(Base):
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
 
-    monitors: Mapped[list["Monitor"]] = relationship(back_populates="user")
+    monitors: Mapped[list["Monitor"]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+
+    refresh_tokens: Mapped[list["RefreshToken"]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
