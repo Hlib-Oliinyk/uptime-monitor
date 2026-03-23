750106f6afb884c49cd558751384b11fdb666e72
Олійник Гліб <glib070707@gmail.com>
2026-03-23 21:00:17 +0200

    feat: add token repository

---
+++ app/repositories/token.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timezone, timedelta
+
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm.sync import update
+
+from app.models.refresh_token import RefreshToken
+
+
+class TokenRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_validate_refresh_token(self, token: str) -> RefreshToken | None:
+        stmt = select(RefreshToken).where(
+            RefreshToken.is_revoked == False,
+            RefreshToken.token == token,
+            RefreshToken.expired_at > datetime.now(timezone.utc)
+        )
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def save_token(self, **data) -> RefreshToken:
+        refresh_tone = RefreshToken(**data)
+        self.db.add(refresh_tone)
+        await self.db.commit()
+        await self.db.refresh(refresh_tone)
+        return refresh_tone
+
+    async def rotate_token_data(
+        self,
+        old_token_id: int,
+        user_id: int,
+        new_token_hash: str
+    ) -> RefreshToken:
+        await self.db.execute(
+            update(RefreshToken)
+            .where(RefreshToken.id == old_token_id)
+            .value(is_revoked = True)
+        )
+
+        new_refresh_token = RefreshToken(
+            user_id=user_id,
+            token=new_token_hash,
+            expired_at=datetime.now(timezone.utc) + timedelta(days=14)
+        )
+
+        self.db.add(new_refresh_token)
+        await self.db.commit()
+        await self.db.refresh(new_refresh_token)
+        return new_refresh_token
+
+    async def delete_token(self, token_hash: str) -> bool:
+        stmt = (update(RefreshToken)
+                .where(RefreshToken.token == token_hash)
+                .values(is_revoked = True))
+        await self.db.execute(stmt)
+        await self.db.commit()
+        return True
--- app/securities/hashing.py
+++ app/securities/hashing.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from passlib.context import CryptContext
 
 
@@ -9,3 +11,7 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
+
+
+def hash_token(token: str) -> str:
+    return hashlib.sha256(token.encode("utf-8")).hexdigest()
